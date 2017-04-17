5ab87e11d2746a2caee0493085bccdf6f5361ffc
Arda Sahiner <sahinera@berkeley.edu>
2017-04-17 04:10:46 -0700

    shhh

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Add all modules to import list
 var imports = [];
 imports.push("mainController");
+var imports2 = []
+imports2.push("howsoft");
 
 angular.module('musicWebsite', imports);
+angular.module('alt', imports2)
